a6913a436f3cc2df4b1e41cb933ed95a90e5d032
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-02-15 10:49:51 +0100

    added control on query string

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 $paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, nulla dolores sapiente velit a aspernatur laborum nisi vitae debitis non mollitia blanditiis, dignissimos autem assumenda minus, quos totam architecto quisquam.
 ';
-$censure = $_GET['censure'];
-$censured=str_replace($censure,'***', $paragraph);
+if(key_exists("censure", $_GET)){
+    $censure = $_GET['censure'];
+    $censured=str_replace($censure,'***', $paragraph);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,11 +22,16 @@ $censured=str_replace($censure,'***', $paragraph);
         $paragraph
     ); ?> caratteri</p>
     <hr />
-    <h3>Censurando la parola che l'utente ci invia come query</h3>
-    <p><?php echo $censured?></p>
-    <p>il paragrafo è composto da <?php echo strlen(
-        $censured
-    ); ?> caratteri</p>
-
+    <?php if(isset($censure)){ ?>
+        <h3>Censurando la parola che l'utente ci invia come query</h3>
+        <p><?php echo $censured?></p>
+        <p>il paragrafo è composto da <?php echo strlen(
+            $censured
+        ); ?> caratteri</p>
+    <?php }else{
+        echo "Per Favore inserisci una query string di tipo ?censure=parolache vuoi censurare.
+        NB va inserita attaccata all'url della pagina";
+    }
+?>
 </body>
 </html>
